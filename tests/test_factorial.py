c91e5191906150ce5f9f3bcd185350a878a195b1
Joel Malonda <malondabeltran15@gmail.com>
2024-02-18 18:49:17 +0100

    commit

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,16 +1,20 @@
 import pytest
 from src.factorial import *
 
+
 def test_factorial():
     assert calcular_factorial(1) == 1
+
 
 def test_TypeError():
     with pytest.raises(TypeError):
         calcular_factorial("aaaa")
 
+
 def test_valueError():
     with pytest.raises(ValueError):
         calcular_factorial(-1)
-    
+
+
 def test_factorial_de_cinc():
     assert calcular_factorial(5) == 120
