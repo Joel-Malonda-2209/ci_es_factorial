e9e0b059d6674be6cd7f76bcafaf353d31c9ce17
Joel Malonda <malondabeltran15@gmail.com>
2024-02-18 18:40:18 +0100

    commit

--- factorial.py
+++ factorial.py
@@ -1,12 +1,18 @@
+"""mòdul càlcul factorial"""
 def calcular_factorial(num):
+    """
+    funció per a calcular el factorial
+
+
+    entrada: 
+    - enter: num
+    eixida:
+    - enter: resultat de calcular el factorial
+    """
     if num == 1:
-        return 1
-    
+        return 1    
     if not isinstance(num,int):
         raise TypeError("Error, te que ser un número")
-
     if num < 0:
         raise ValueError("Error de número negatiu")
-
-    else:
-        return num * calcular_factorial(num - 1)
+    return num * calcular_factorial(num - 1)
